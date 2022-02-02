3ebf28221deada5714e84e386895bf4aa5dcaab9
nOy39 <popov.a39rus@gmail.com>
2022-02-02 07:01:39 +0300

    next 3 commit

---
+++ basic_part_1/additional_part/os_mod_t.py
@@ -0,0 +1,6 @@
+import os
+
+print(os.getcwd())
+print(os.path.relpath('/home/andrey/repo/python/python_task/', start=None))
+print('breakpoint')
+os.path.relpath('/home/andrey/repo/python/python_task/', start=None)
---
+++ basic_part_1/e31_60/ex_41.py
@@ -0,0 +1,4 @@
+"""Write a Python program to check whether a file exists."""
+import os
+
+print(os.path.isfile('../../README.md'))
