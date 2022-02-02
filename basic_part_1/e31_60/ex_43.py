068f7e97ec2b8d1365664f9ceccb2e485cc05705
Andrey Popov <popov.a39rus@gmail.com>
2022-02-02 11:41:25 +0300

    new commit

---
+++ basic_part_1/e31_60/ex_43.py
@@ -0,0 +1,10 @@
+"""Write a Python program to get OS name, platform and release information."""
+
+# Use module platform.
+
+
+from platform import uname
+
+os_data = uname()
+print('OS name: %s, platform: %s, release: %s' %(os_data[0], os_data[2], os_data[3]))
+
---
+++ basic_part_1/e31_60/ex_45.py
@@ -0,0 +1,10 @@
+"""Write a python program to call an external command in Python"""
+import platform, os, subprocess
+
+type_os = platform.platform()[:3].lower()
+print(platform.platform())
+
+if type_os != 'win':
+    subprocess.call(["ping", "c -2", "www.ya.ru"])
+else:
+    os.system('calc')
