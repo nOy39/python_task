d13f02a30ed2aadb9e09ad6a8d8186fcffba8d5d
Andrey Popov <popov.a39rus@gmail.com>
2022-02-03 20:31:58 +0300

    again commit ^)

---
+++ basic_part_1/e61_90/ex_63.py
@@ -0,0 +1,7 @@
+"""Write a Python program to get an absolute file path"""
+
+
+from os import path
+# print(os.path.abspath('ex_63.py'))
+print('absolute path to fo file ex_63.py')
+print(path.abspath('ex_63.py'))
---
+++ basic_part_1/e61_90/ex_65.py
@@ -0,0 +1,3 @@
+"""Write a Python program to convert seconds to day, hour, minutes and seconds"""
+
+import time
