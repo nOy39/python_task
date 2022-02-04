0a2dffb254c2c6205e83631c88d3407da497191f
nOy39 <popov.a39rus@gmail.com>
2022-02-04 19:21:50 +0300

    tasks

--- basic_part_1/e61_90/ex_70.py
+++ basic_part_1/e61_90/ex_70.py
@@ -1,10 +1,7 @@
-"""Write a Python program to sort three integers without using conditional statements and loops."""
+"""TODO"""
+"""Write a Python program to sort files by date."""
 
-x = int(input('First number: '))
-y = int(input('Second number: '))
-z = int(input('Third number: '))
 
-a_min = min(x, y, z)
-c_max = max(x, y, z)
-b_mid = (x + y + z) - (a_min + c_max)
-print(f'{a_min}, {b_mid}, {c_max}')
+import os
+path = '/home/andrey/Downloads/'
+os.environ
---
+++ basic_part_1/e61_90/ex_72.py
@@ -0,0 +1,4 @@
+"""Write a Python program to get the details of math module"""
+import math
+print(dir(math))
+help(math)
