f1926157ba945deb66e7f9a20075d3242138a70a
Andrey Popov <popov.a39rus@gmail.com>
2022-02-07 13:37:46 +0300

    newest

--- code_wars/replace_with_alphabet_position.py
+++ code_wars/replace_with_alphabet_position.py
@@ -7,11 +7,22 @@
 
 
 from collections import *
+
+
+# def alphabet_position(text):
+#     alphabet = 'abcdefghijklmnopqrstuvwxyz'
+#     list_of_char = list(text)
+#     result = list()
+#     for c in list_of_char:
+#         if c.isalpha():
+#             result.append(alphabet.find(c.lower()))
+#     return " ".join(str(e) for e in result)
+
+
 def alphabet_position(text):
-    pass
+    return ' '.join(str(ord(c) - 96) for c in text.lower() if c.isalpha())
 
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-alpha_list = list(alphabet)
-dict_alpha = OrderedDict.fromkeys(alpha_list)
-print(dict_alpha)
+print(alphabet_position('Aneofthi'))
+print(alphabet_position('The sunset sets at twelve o\'clock.'))
+print("20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11")
