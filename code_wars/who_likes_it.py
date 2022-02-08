aae7cd089503587eb0f7f20a9c39bfc00bb65633
Andrey Popov <popov.a39rus@gmail.com>
2022-02-08 16:05:01 +0300

    newest

--- code_wars/who_likes_it.py
+++ code_wars/who_likes_it.py
@@ -1,18 +1,30 @@
 """
-You probably know the "like" system from Facebook and other pages.
-People can "like" blog posts, pictures or other items.
-We want to create the text that should be displayed next to such an item.
-
 Implement the function which takes an array containing the names of people that like an item.
 It must return the display text as shown in the examples:
+
+[]                                -->  "no one likes this"
+["Peter"]                         -->  "Peter likes this"
+["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+Note: For 4 or more names, the number in "and 2 others" simply increases.
 """
 
 
 def likes(names):
-    s = ' like this'
-    if names.__len__() > 3:
-        s = names([0]) + s
-    return s
+    template = ''
+    case = len(names)
+    if case == 0:
+        template = 'no one like this'
+    elif case == 1:
+        template = f'{names[0]} like this'
+    elif case == 2:
+        template = f'{names[0]} and {names[1]} like this'
+    elif case == 3:
+        template = f'{names[0]}, {names[1]} and {names[2]} like this'
+    else:
+        template = f'{names[0]}, {names[1]} and 2 other like this'
+    return template
 
 
 print(likes(["Alex", "Jacob", "Mark", "Max"]))
