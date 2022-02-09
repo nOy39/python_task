b3b47627d0c018106895250282e120963e28e6c0
Andrey Popov <popov.a39rus@gmail.com>
2022-02-09 14:20:05 +0300

    newest

---
+++ code_wars/where_my_anagrams_at.py
@@ -0,0 +1,22 @@
+"""
+Write a function that will find all the anagrams of a word from a list.
+You will be given two inputs a word and an array with words.
+You should return an array of all the anagrams or an empty array if there are none.
+
+For example:
+anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+"""
+
+
+def anagrams(word, words_list):
+    result = []
+    for w in words_list:
+        if sorted(word) == sorted(w):
+            result.append(w)
+    return result   # [w for w in words_list if sorted(word) == sorted(w)]
+
+
+print(anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']))
+print(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']))
