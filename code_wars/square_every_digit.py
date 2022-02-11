7c63371a276d827f48131e5aff9b7afb5ece22fc
nOy39 <popov.a39rus@gmail.com>
2022-02-12 00:46:02 +0300

    Square every digit

--- code_wars/square_every_digit.py
+++ code_wars/square_every_digit.py
@@ -1,19 +1,10 @@
 """In this kata, you are asked to square every digit of a number and concatenate them.
 
     For example, if we run 9119 through the function, 811181 will come out,
-    because 92 is 81 and 12 is 1.
+    because 9**2 is 81 and 1**2 is 1.
 Note: The function accepts an integer and returns an integer"""
 
 
-# My
 def square_digits(num):
-    result = ''
-    for i in num.__str__():
-        result += int(i).__pow__(2).__str__()
-    return int(result)
+    return "".join(str(int(n) ** 2) for n in str(num))
 
-    print(result)
-
-
-square_digits(9119)
-print("END")
