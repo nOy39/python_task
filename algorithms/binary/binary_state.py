1f23b95a9402ae10cd166e9d7435d9893c511dff
Andrey Popov <popov.a39rus@gmail.com>
2022-02-14 20:59:54 +0300

    algorithms

---
+++ algorithms/binary/binary_state.py
@@ -0,0 +1,10 @@
+arr = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+
+def binary_search(array, item):
+    low = 0
+    high = len(array)
+    return True
+
+
+print(binary_search(arr, 10))
