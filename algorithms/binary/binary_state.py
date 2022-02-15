05c16f9c2f3f7533aeb4373075bdeb55131db408
Andrey Popov <popov.a39rus@gmail.com>
2022-02-15 12:20:12 +0300

    algorithms

--- algorithms/binary/binary_state.py
+++ algorithms/binary/binary_state.py
@@ -1,10 +1,13 @@
-arr = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+arr = [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 
 
 def binary_search(array, item):
     low = 0
     high = len(array)
+    while(True):
+        if array[high/2] < item:
+            high /= 2
     return True
 
 
-print(binary_search(arr, 10))
+print(binary_search(arr, 7))
